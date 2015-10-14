be2f2c8bc3170e435fafec86644ecb8f6cb373ca
Chyngyz <camphosis@gmail.com>
2015-10-14 20:54:29 +0600

    Slider in progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+(function($){
+	$(document).ready(function() {
+
+		$('#menu-trigger').click(function(e){
+			e.preventDefault();
+			$('#main-menu').toggleClass('show');
+		});
+
+		$('#topmenu-trigger').click(function(e){
+			e.preventDefault();
+			$('#top-menu').toggleClass('show');
+		})
+
+ 
+	  	$("#hero-slider").owlCarousel({
+	 
+		    navigation : false, // Show next and prev buttons
+		    slideSpeed : 300,
+		    paginationSpeed : 400,
+		    singleItem:true
+		 
+		    // "singleItem:true" is a shortcut for:
+		    // items : 1, 
+		    // itemsDesktop : false,
+		    // itemsDesktopSmall : false,
+		    // itemsTablet: false,
+		    // itemsMobile : false
+	 
+	  	});
+	 
+	});
+
+})(jQuery);
